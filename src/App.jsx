0a385774050c10a222f8f46211566982b48128ca
VIVEK Aggarwal <vivek@VIVEKs-MacBook-Air-2.local>
2026-02-28 16:42:10 +0530

    Add base CSS and App component shell

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import VendorRegister from './pages/VendorRegister';
+import VendorProfile from './pages/VendorProfile';
+import Blogs from './pages/Blogs';
+import BlogPost from './pages/BlogPost';
+import About from './pages/About';
+import './App.css';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="app">
+        <Navbar />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/register" element={<VendorRegister />} />
+            <Route path="/vendor/:id" element={<VendorProfile />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
